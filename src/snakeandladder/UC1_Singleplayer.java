5d4b1ee269b5c1782761dfb7d7e8ff18797165c3
susmitha <susmithareddychinthala024@gmail.com>
2022-10-07 20:42:23 +0530

    Single player at start position

---
+++ src/snakeandladder/UC1_Singleplayer.java
@@ -0,0 +1,14 @@
+package snakeandladder;
+    import java.util.Random;
+
+public class UC1_Singleplayer {
+
+        public static void main(String[] args) {
+            int pos = 0;
+            System.out.println("Welcome to Snake And Ladder program");
+            System.out.println("Single player at start position "+ pos);
+
+        }
+
+    }
+
